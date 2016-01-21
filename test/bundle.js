386cf0f5e339e4cf3d89e1dab0874294b0b7dd6b
Alex Buchanan <buchanae@gmail.com>
2016-01-22 09:15:27 +1100

    show selected json in test page

--- test/bundle.js
+++ test/bundle.js
@@ -37672,6 +37672,7 @@ function extend(target) {
 
 },{}],573:[function(require,module,exports){
 'use strict';
+
 require('babel-polyfill');
 var React = require('react');
 var reactDom = require('react-dom');
@@ -37681,6 +37682,31 @@ var LocationSearch = dist.LocationSearch;
 var Mapbox_access_key = 'pk.eyJ1IjoiYnVjaGFuYWUiLCJhIjoiY2loNzR0Y3U5MGd2OXZka3QyMHJ5bXo0ZCJ9.HdT8S-gTjPRkTb4v8Z23KQ';
 
 var container = document.getElementById('container');
-reactDom.render(React.createElement(LocationSearch, { Mapbox_access_key: Mapbox_access_key }), container);
+
+var Main = React.createClass({
+  getInitialState() {
+    return {'selected': ''};
+  },
+  render() {
+    var main = this;
+
+    return React.createElement('div', {},
+      React.createElement(LocationSearch, {
+        Mapbox_access_key: Mapbox_access_key,
+        onResultSelected: function(result) {
+          main.setState({
+            selected: JSON.stringify(result, null, 2)
+          });
+        }
+      }),
+      React.createElement('textarea', {
+        className: 'selected-json',
+        value: this.state.selected
+      })
+    );
+  }
+});
+
+reactDom.render(React.createElement(Main), container);
 
 },{"../dist":148,"babel-polyfill":150,"react":526,"react-dom":1}]},{},[573]);
--- test/entry.js
+++ test/entry.js
@@ -1,4 +1,5 @@
 'use strict';
+
 require('babel-polyfill');
 var React = require('react');
 var reactDom = require('react-dom');
@@ -8,4 +9,29 @@ var LocationSearch = dist.LocationSearch;
 var Mapbox_access_key = 'pk.eyJ1IjoiYnVjaGFuYWUiLCJhIjoiY2loNzR0Y3U5MGd2OXZka3QyMHJ5bXo0ZCJ9.HdT8S-gTjPRkTb4v8Z23KQ';
 
 var container = document.getElementById('container');
-reactDom.render(React.createElement(LocationSearch, { Mapbox_access_key: Mapbox_access_key }), container);
+
+var Main = React.createClass({
+  getInitialState() {
+    return {'selected': ''};
+  },
+  render() {
+    var main = this;
+
+    return React.createElement('div', {},
+      React.createElement(LocationSearch, {
+        Mapbox_access_key: Mapbox_access_key,
+        onResultSelected: function(result) {
+          main.setState({
+            selected: JSON.stringify(result, null, 2)
+          });
+        }
+      }),
+      React.createElement('textarea', {
+        className: 'selected-json',
+        value: this.state.selected
+      })
+    );
+  }
+});
+
+reactDom.render(React.createElement(Main), container);
